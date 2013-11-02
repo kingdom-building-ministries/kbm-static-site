dc252d2cd35a79ffa46694e5e9eec86a62b76a11
Zach Starkebaum <zach@relevantwalk.com>
2013-11-02 13:40:24 -0600

    Testing a fix for travis ci

--- _plugins/gallery.rb
+++ _plugins/gallery.rb
@@ -1,3 +1,7 @@
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+
 begin
   require 'exifr'
 rescue LoadError
@@ -12,7 +16,7 @@ module Jekyll
       # Add a new array to contain all the items in the gallery
       #
       attr_accessor :photos
-      
+
       alias orig_site_payload site_payload
       def site_payload
         hash = orig_site_payload
@@ -24,26 +28,26 @@ module Jekyll
         hash
       end
   end
-  
+
   class Thumbnail < StaticFile
-  
+
     def initialize(site, source, dir, name)
       @site = site
       @dir = dir
       @name = name
       @path = File.join(source, dir, "_photos", name)
     end
-    
+
     def path
       return @path
     end
-    
+
     def write(dest)
       target_path = destination(dest)
-      
+
       return false if File.exist?(target_path) and !modified?
       @@mtimes[path] = mtime
-      
+
       FileUtils.mkdir_p(File.dirname(target_path))
       img = Magick::ImageList.new(@path)
       thumb = img.resize_to_fill(75, 75)
@@ -51,12 +55,12 @@ module Jekyll
       puts ("Thumbnail:" + target_path)
       true
     end
-    
+
     def url
       WEBrick::HTTPUtils.escape_path(destination(''))
     end
-    
-    def destination(dest) 
+
+    def destination(dest)
       rootImageDirectory = @site.config['photo_dest_dir'] || 'img'
       dest_string = File.join(dest, rootImageDirectory, @dir, @name)
       d = Pathname.new(dest_string)
@@ -65,25 +69,25 @@ module Jekyll
       path
     end
   end
-  
+
   class Photo < StaticFile
     attr_accessor :name
     attr_accessor :image_url
     attr_accessor :albums
     attr_accessor :thumbnail
-    
+
     def initialize(site, source, dir, name)
       @site = site
       @dir = dir
       @path = File.join(source, dir, "_photos", name)
       self.name = name
       self.image_url = WEBrick::HTTPUtils.escape_path(destination(''))
-      
+
       self.thumbnail = Thumbnail.new(site, source, dir, name)
-      
+
       self.albums = dir.downcase.split('/').reject { |x| x.empty? }
     end
-  
+
     def to_liquid
       {
         "name" => self.name,
@@ -92,21 +96,21 @@ module Jekyll
         "albums" => self.albums
       }
     end
-    
+
     def path
       return @path
     end
-  
+
     def destination(dest)
       rootImageDirectory = @site.config['photo_dest_dir'] || 'img'
       File.join(dest, rootImageDirectory, @dir, self.name)
     end
-    
+
     def <=>(other)
       cmp = self.name <=> other.name
       return cmp
     end
-  
+
     # Returns the object as a debug String.
     def inspect
       "#<Photo @name=#{self.name.inspect}>"
@@ -115,19 +119,19 @@ module Jekyll
 
   class GalleryGenerator < Generator
   	safe true
-  	
+
   	def generate(site)
   	  site.photos = []
   		recursively_read_directories(site)
-  	end 
-  	
+  	end
+
   	def recursively_read_directories(site, dir = '')
   	  base = File.join(site.source, dir)
   	  entries = Dir.chdir(base) { site.filter_entries(Dir.entries('.')) }
-  	
+
   	  read_photos(site, dir)
   	  site.photos.sort!
-  	
+
   	  entries.each do |f|
   	    f_abs = File.join(base, f)
   	    if File.directory?(f_abs)
@@ -136,7 +140,7 @@ module Jekyll
   	    end
   	  end
   	end
-  	
+
   	def read_photos(site, dir)
   	  entries = site.get_entries(dir, '_photos')
   	  entries.each do |f|
